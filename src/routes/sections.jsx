0559986635766ece92c910ff159ab8dd19a2c547
Daniel Abera <dsichale@gmail.com>
2023-12-02 01:54:21 +0300

    edited

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -6,10 +6,8 @@ import { useAuth } from 'src/context/AuthContext';
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
 export const VotePage = lazy(() => import('src/pages/vote'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
